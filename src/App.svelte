36dd5bac8d6f6abda21c4e38ac4903a8b4c8c1fb
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-22 14:41:20 -0600

    Made basic markup.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,69 @@
-<script>
-// Props
-export let recipient;
-</script>
+<header>
+  <h1 lang="cr">itwêwina</h1>
+  <h2>Plains Cree Dictionary</h2>
+</header>
 
-<h1>Hello {recipient}!</h1>
+<nav>
+  <form action="search" method="GET">
+    <input type="search" placeholder="Search...">
+  </form>
+</nav>
+
+<main>
+  <article>
+    <dl>
+      <dt><dfn lang="cr">itwêw</dfn> (Noun)</dt>
+      <dd>
+        <ol>
+          <li>He says <cite>MD</cite></li>
+          <li>s/he says so, s/he says thus, s/he calls (it) so; it has such a
+          meaning. <cite>CW</cite></li>
+        </ol>
+
+        <section>
+          <table>
+            <tbody>
+              <tr> <th colspan="2"> <b>Basic forms</b> (ni-/ki- word) </th> </tr>
+
+              <tr> <td>I</td> <td lang="cr">nititwân</td> </tr>
+              <tr> <td>You</td> <td lang="cr">kititwân</td> </tr>
+              <tr> <td>S/he</td> <td lang="cr">itwêw</td> </tr>
+              <tr> <td>We</td> <td lang="cr">nititwânân</td> </tr>
+              <tr> <td rowspan="2">You and We</td> <td> <span lang="cr">kititwânaw</span> or</td> </tr>
+              <tr>                                 <td lang="cr">kititwânânaw</td> </tr>
+              <tr> <td>You (all)</td> <td lang="cr">kititwânânâw</td> </tr>
+
+              <tr> <th colspan="2"> <b>Basic forms</b> (ê-/kâ- word) </th> </tr>
+
+              <tr> <td>I</td> <td lang="cr">ê-itwêyân</td> </tr>
+              <tr> <td>You</td> <td lang="cr">ê-itwêyan</td> </tr>
+              <tr> <td>S/he</td> <td lang="cr">ê-itwêt</td> </tr>
+              <tr> <td>We</td> <td lang="cr">ê-itwêyâhk</td> </tr>
+              <tr> <td>You and We</td> <td><span lang="cr">ê-itwêyahk</span></td> </tr>
+              <tr> <td>You (all)</td> <td lang="cr">ê-itwêyêk</td> </tr>
+            </tbody>
+          </table>
+
+          <a href="#">More</a>
+        </section>
+
+        <section>
+          <p><a href="#">View similar words</a></p>
+        </section>
+      </dd>
+    </dl>
+  </article>
+</main>
+
+<footer>
+  <a href="https://www.maskwacised.ca/"><img src="mesc-logo.png" alt="MESC"></a>
+  <a href="https://altlab.ualberta.ca/"><img src="uofa-logo.png" alt="University of Alberta"></a>
+  <a href="http://fnuniv.ca/"><img src="uofa-logo.png" alt="FNU"></a>
+</footer>
+
+
+<style>
+td[rowspan] {
+  vertical-align: top;
+}
+</style>
