6907c548271211b718d38d86c132295c8eba08a9
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-13 14:02:51 -0600

    Delete itwew VAI defintion; replace with niska.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,5 @@
 <script>
   import Cree from './components/Cree.svelte';
-  import OrthographySelector from './components/OrthographySelector.svelte';
   import CiteDict from './components/CiteDict.svelte';
 </script>
 
@@ -17,61 +16,29 @@
 
 <main>
   <article>
-    <dl>
-      <dt><dfn><Cree sro="itwêw" /></dfn> (Noun)</dt>
-      <dd>
-        <section class="recordings">
-          <button class="play-audio-button">▶️</button>
-          <audio preload="none">
-             <source src="https://sapir.artsrn.ualberta.ca/validation/recording/29444265281262d0d7e40e05b52c1c84cfda609bed83e6fa7cc07952045a2f61.m4a">
-          </audio>
-            “<Cree sro="minôs" />” (Harley Simon, Maskwacis)
-        </section>
+    <header>
+      <h1><dfn><Cree sro="itwêw" /></dfn> <span class="part-of-speech">(Noun)</span></h1>
+    </header>
+    <section class="translations">
+      <ol>
+        <li>goose <CiteDict source="CW"/> <CiteDict source="MD"/></li>
+      </ol>
+    </section>
 
-        <section class="translations">
-          <ol>
-            <li>He says <CiteDict source="MD"/></li>
-            <li>s/he says so, s/he says thus, s/he calls (it) so; it has such a meaning. <CiteDict source="CW"/></li>
-          </ol>
-        </section>
+    <section class="paradigm">
+      <table>
+        <tbody>
+          <tr> <th colspan="2"> <b>Basic forms</b> (ni-/ki- word) </th> </tr>
 
-        <section class="paradigm">
-          <table>
-            <tbody>
-              <tr> <th colspan="2"> <b>Basic forms</b> (ni-/ki- word) </th> </tr>
+          <tr> <td>One</td>                       <td><Cree sro="nititwân" /></td> </tr>
+          <tr> <td>Many</td>                     <td><Cree sro="kititwân" /></td> </tr>
+          <tr> <td>Further</td>                    <td><Cree sro="itwêw" /></td> </tr>
+        </tbody>
+      </table>
 
-              <tr> <td>I</td>                       <td><Cree sro="nititwân" /></td> </tr>
-              <tr> <td>You</td>                     <td><Cree sro="kititwân" /></td> </tr>
-              <tr> <td>S/he</td>                    <td><Cree sro="itwêw" /></td> </tr>
-              <tr> <td>We</td>                      <td><Cree sro="nititwânân" /></td> </tr>
-              <tr> <td rowspan="2">You and We</td>  <td><Cree sro="kititwânaw" /> or</td> </tr>
-              <tr>                                  <td><Cree sro="kititwânânaw" /></td> </tr>
-              <tr> <td>You (all)</td>               <td><Cree sro="kititwânâwâw" /></td> </tr>
-
-              <tr> <th colspan="2"> <b>Basic forms</b> (ê-/kâ- word) </th> </tr>
-
-              <tr> <td>I</td>                       <td><Cree sro="ê-itwêyân" /></td> </tr>
-              <tr> <td>You</td>                     <td><Cree sro="ê-itwêyan" /></td> </tr>
-              <tr> <td>S/he</td>                    <td><Cree sro="ê-itwêt" /></td> </tr>
-              <tr> <td>We</td>                      <td><Cree sro="ê-itwêyâhk" /></td> </tr>
-              <tr> <td>You and We</td>              <td><Cree sro="ê-itwêyahk" /></td> </tr>
-              <tr> <td>You (all)</td>               <td><Cree sro="ê-itwêyêk" /></td> </tr>
-            </tbody>
-          </table>
-
-          <a href="more">More</a>
-        </section>
-
-        <section>
-          <p><a href="similar-to">View similar words</a></p>
-        </section>
-      </dd>
-    </dl>
+      <a href="more">More</a>
+    </section>
   </article>
-
-  <aside>
-    <OrthographySelector/>
-  </aside>
 </main>
 
 <footer>
