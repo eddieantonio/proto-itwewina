2e6f8bda31e311487af799002d65366f3a943472
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-24 10:52:19 -0600

    Factor out header.

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,10 @@
 <script>
-  import Cree from './components/Cree.svelte';
+  import Header from './components/Header.svelte';
+  import Footer from './components/Footer.svelte';
 
   import DefaultScreen from './screens/Default.svelte';
   import HomeScreen from './screens/Home.svelte';
   
-  import Footer from './components/Footer.svelte';
-
   // TODO: make this work!
   const screens = {
     'default': DefaultScreen,
@@ -14,13 +13,7 @@
   let screen = screens['home'];
 </script>
 
-<header class="application-header">
-  <a href="/">
-    <h1 class="application-header__heading application-title"><Cree sro="itwêwina" /></h1>
-    <h2 class="application-header__heading application-subtitle">Plains Cree Dictionary</h2>
-  </a>
-</header>
 
+<Header />
 <svelte:component this={HomeScreen} />
-
 <Footer />
---
+++ src/components/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Cree from '../components/Cree.svelte';
+</script>
+
+<header class="application-header">
+  <a href="/">
+    <h1 class="application-header__heading application-title"><Cree sro="itwêwina" /></h1>
+    <h2 class="application-header__heading application-subtitle">Plains Cree Dictionary</h2>
+  </a>
+</header>
+
