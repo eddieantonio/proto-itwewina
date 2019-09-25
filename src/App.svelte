362a23eb0f1ad21ea6396e532e79f3188e00d863
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-25 12:32:12 -0600

    Allow for switching screen and orthography using query params.

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,37 @@
   import DefaultScreen from './screens/Default.svelte';
   import HomeScreen from './screens/Home.svelte';
 
-  // TODO: make this work!
+  import {orthography} from './components/Cree/store';
+  import {SRO_MACRON, SRO_CIRCUMFLEX, SYLLABICS} from './components/Cree';
+
   const screens = {
     'default': DefaultScreen,
     'home': HomeScreen
   };
-  let screen = screens['home'];
+
+  const orthographies = {
+    [SYLLABICS]: SYLLABICS,
+    [SRO_CIRCUMFLEX]: SRO_CIRCUMFLEX,
+    [SRO_MACRON]: SRO_MACRON
+  };
+
+  /* Figure out screen and orthography from query string: */
+  let params = new URLSearchParams(window.location.search);
+  let desiredScreen = params.get('s') || 'default';
+  let desiredOrth = params.get('o') || SRO_CIRCUMFLEX;
+
+  if (!(desiredScreen in screens)) {
+    console.warn(`${desiredScreen} is not a valid screen; using default.`);
+    desiredScreen = 'default';
+  }
+
+  if (!(desiredOrth in orthographies)) {
+    console.warn(`${desiredOrth} is not a valid orthography; using sro-c.`);
+    desiredScreen = SRO_CIRCUMFLEX;
+  }
+
+  let screen = screens[desiredScreen];
+  orthography.set(desiredOrth)
 </script>
 
 
