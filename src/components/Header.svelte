059f8f5309f6c9b09ac0231335f6fd492044886b
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-25 10:31:59 -0600

    Change application-header to top-bar and branding blocks.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,10 +2,20 @@
   import Cree from '../components/Cree.svelte';
 </script>
 
-<header class="application-header">
-  <a href="/">
-    <h1 class="application-header__heading application-title"><Cree sro="itwêwina" /></h1>
-    <h2 class="application-header__heading application-subtitle">Plains Cree Dictionary</h2>
-  </a>
-</header>
+<div class="top-bar application__header">
+  <header class="branding top-bar__heading ">
+    <a href="/">
+      <h1 class="branding__heading branding__title"><Cree sro="itwêwina" /></h1>
+      <p class="branding__heading branding__subtitle">Plains Cree Dictionary</p>
+    </a>
+  </header>
+
+  <nav class="top-bar__nav">
+    <details>
+      <summary>
+        English (êîôâ)
+      </summary>
+    </details>
+  </nav>
+</div>
 
