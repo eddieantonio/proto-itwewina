c4706d76f6adeb0939183a0c0e0a023b00c84dc0
Eddie Antonio Santos <easantos@ualberta.ca>
2019-09-26 15:18:25 -0600

    Figure out how toggle-box menus work.

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -11,10 +11,45 @@
   </header>
 
   <nav class="top-bar__nav">
-    <details>
-      <summary>
+    <details class="toggle-box toggle-box--with-menu" open>
+      <summary class="toggle-box__toggle">
         English (êîôâ)
       </summary>
+      <div class="menu toggle-box__menu">
+        <fieldset class="menu__category">
+          <legend class="menu__caption"> Language </legend>
+
+          <label class="menu-choice">
+            <input class="menu-choice__input" type="radio" name="lang" value="en" checked>
+            <span class="menu-choice__label"> English </span>
+          </label>
+
+          <label class="menu-choice">
+            <input class="menu-choice__input" type="radio" name="lang" value="cr">
+            <span class="menu-choice__label"> Cree/<Cree sro="nêhiyawêwin"/> </span>
+          </label>
+        </fieldset>
+
+        <fieldset class="menu__category">
+          <legend class="menu__caption"> Show Cree words in </legend>
+
+          <label class="menu-choice">
+            <input class="menu-choice__input" type="radio" name="orth" value="Latn" checked>
+            <span class="menu-choice__label"> SRO (êîôâ) </span>
+          </label>
+
+          <label class="menu-choice">
+            <input class="menu-choice__input" type="radio" name="orth" value="Latn-x-macron">
+            <span class="menu-choice__label"> SRO (ēīōā) </span>
+          </label>
+
+          <label class="menu-choice">
+            <input class="menu-choice__input" type="radio" name="orth" value="Latn">
+            <span class="menu-choice__label"> Syllabics (ᒐᐦᑭᐯᐦᐃᑲᓇ) </span>
+          </label>
+        </fieldset>
+
+      </div>
     </details>
   </nav>
 </div>
